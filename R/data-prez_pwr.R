059cca820202e8327a8da2417d005ab282d62b69
dataHunterTest <montanatri@yahoo.com>
2021-05-12 10:34:48 -0700

    added prez_pwr dataset

---
+++ R/data-prez_pwr.R
@@ -0,0 +1,23 @@
+#'  prez_pwr.
+#'
+#'  Data from a Pew Research Center poll about Presidential
+#'  power/control over gas prices.
+#'
+#' @format A data frame with 365 rows and 3 variables.
+#' \describe{
+#'   \item{president}{Sitting President at time of the poll.}
+#'   \item{party}{Political party of the respondent with levels d(emocrat)
+#'   and r(epublican).}
+#'   \item{has_pwr}{Respondent answer to the question: "Is
+#'   the price of gasoline something the president can do
+#'   alot about, or is that beyond the president's control?"}
+#' @examples
+#'  library(ggplot2)
+#'  ggplot(prez_pwr, aes(has_pwr))+
+#'    geom_bar()
+#'
+#' @source
+#'   [Pew Research Center, May 2006 & March 2012](https://www.pewresearch.org/fact-tank/2013/07/25/can-a-president-control-gas-prices-depends-on-when-you-ask/).
+#'
+#'
+"prez_pwr"
