113ada23fa8f9857b7fd6695b3b120ebd8da0c8e
npaterno <nicholas.paterno@gmail.com>
2021-05-23 14:32:08 -0700

    cleaned a bit more and completed documentation

--- R/data-blizzard_salary.R
+++ R/data-blizzard_salary.R
@@ -14,10 +14,24 @@
 #'   \item{location}{Current office of employment.}
 #'   \item{performance_rating}{Most recent review performance rating.}
 #' }
+#' @examples 
+#' library(ggplot2)
+#' library(dplyr)
 #'
+#'plot_data <- blizzard_salary %>% 
+#'mutate(annual_salary = case_when(
+#'salary_type == "week" ~ current_salary*52,
+#'salary_type == "hour" ~ current_salary*40*52,
+#'TRUE ~ current_salary
+#'))
 #'
-#'
+#'ggplot(plot_data, aes(annual_salary))+
+#'geom_histogram(binwidth = 25000, color = "white")+
+#'labs(title = "Current Salary of Blizzard Employees",
+#'     x = "Salary",
+#'          y = "Number of Employees")
+#' 
 #' @source
-#'   \href{https://www.bloomberg.com/news/articles/2020-08-03/blizzard-workers-share-salaries-in-revolt-over-wage-disparities}{Bloomberg}
+#'   [Bloomberg](https://www.bloomberg.com/news/articles/2020-08-03/blizzard-workers-share-salaries-in-revolt-over-wage-disparities)
 #'
 "blizzard_salary"
--- data-raw/blizzard_salary/blizzard_salary-dataprep.R
+++ data-raw/blizzard_salary/blizzard_salary-dataprep.R
@@ -11,8 +11,8 @@ raw_data <- read_csv(here::here("data-raw/blizzard_salary/salary.csv"))
 # cleaning: 
 
 blizzard_salary <- raw_data %>% 
-  mutate(current_salary = str_replace(raw_data$current_salary, "[$,]", ""),
-         percent_incr = str_replace(raw_data$percent_incr, "[%]",""),
+  mutate(current_salary = as.numeric(str_replace(raw_data$current_salary, "[$,]", "")),
+         percent_incr = as.numeric(str_replace(raw_data$percent_incr, "[%]","")),
          location = case_when(location == "LA" ~ "Los Angeles",
                               TRUE ~ location))
 
