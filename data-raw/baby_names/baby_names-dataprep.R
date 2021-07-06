4c3fbb8c53f7c90bef9571a6f6609281bfc7d73f
karenjh2 <khig@earthlink.net>
2021-07-06 12:49:32 +1000

    Data set added: baby_names

---
+++ data-raw/baby_names/baby_names-dataprep.R
@@ -0,0 +1,16 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+# load data ---------------------------------------------------------------------
+raw_data <- read.csv(here::here("data-raw/baby_names/baby_names.csv"))
+
+# cleaning ----------------------------------------------------------------------
+
+# no cleaning required
+baby_names<- raw_data
+
+# save --------------------------------------------------------------------------
+
+usethis::use_data(baby_names, overwrite = TRUE)
