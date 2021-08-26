fd8b0970ac8d1ee79fe0234eb3b4fa9c60b9e0aa
npaterno <nicholas.paterno@gmail.com>
2021-08-26 09:03:19 -0700

    saving changes on the branch before pulling main

--- data-raw/gerrymander/gerrymander-dataprep.R
+++ data-raw/gerrymander/gerrymander-dataprep.R
@@ -1,12 +1,27 @@
 # Load packages --------------------------------------------------------------
 library(readr)
+library(dplyr)
+library(tidyr)
+library(janitor)
+library(stringr)
 
 # Load data ------------------------------------------------------------------
 raw_data <- read_csv(here::here("data-raw/gerrymander/isostat_congress_election_gerry.csv"))
 
 # Cleaning: ------------------------------------------------------------------
 
-data_set_name <- raw_data
+clean_data <- raw_data %>% 
+  clean_names() %>% 
+  select(-x2) %>% 
+  separate(col = member2016,
+           into = c("last_name", "first_name"),
+           sep = ", ") %>% 
+  mutate(
+    party16 = str_extract(party16, "[:alpha:]"),
+    party18 = str_extract(party18, "[:alpha:]")
+  )
+
+gerrymander <- clean_data
 
 # Save -----------------------------------------------------------------------
 
