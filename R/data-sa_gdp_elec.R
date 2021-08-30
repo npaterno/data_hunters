dd37a08dac81d6389b717913903f75bdc9db3807
npaterno <nicholas.paterno@gmail.com>
2021-08-30 09:41:39 -0700

    updated documentation

--- R/data-sa_gdp_elec.R
+++ R/data-sa_gdp_elec.R
@@ -2,7 +2,7 @@
 #' 
 #' Includes yearly data on gdp, gni, co2 emissions, start up costs.
 #'
-#' @format A data frame with ___ rows and 7 variables.
+#' @format A data frame with 16 rows and 7 variables.
 #' \describe{
 #'   \item{year}{Year data collected.}
 #'   \item{access_elec}{Access to electricity as a percentage of the population.}
@@ -17,22 +17,15 @@
 #' @examples
 #' library(ggplot2)
 #' ggplot(sa_gdp_elec, aes(year, access_elec))+
-#'    geom_point(alpha = 0.3) +
-#'  labs( x= "Year", y = "% Access to Electricity", title = "% with Access to Electricity in South Africa 2003- 2018") 
+#'  geom_point(alpha = 0.3) +
+#'  labs( x= "Year", 
+#'        y = "Percent of Population", 
+#'        title = "Access to Electricity in South Africa 2003 - 2018") 
 #'
 
-#' @source [World Bank ](https://databank.worldbank.org/source/world-development-indicators)
+#' @source 
+#' [World Bank I](https://databank.worldbank.org/source/world-development-indicators)
+#' [World Bank II](http://www.doingbusiness.org/)
+#' [Carbon Dioxide Information Analysis Center, Environmental Sciences Division, Oak Ridge National Laboratory](https://www.climatewatchdata.org/ghg-emissions.)
 #'
-#'World Bank Global Electrification Database from "Tracking SDG 7: The Energy Progress Report" led jointly by the custodian agencies: the International Energy Agency (IEA), the International Renewable Energy Agency (IRENA), the United Nations Statistics Division (UNSD), the World Bank and the World Health Organization (WHO).
-#'
-#'World Bank, Doing Business project http://www.doingbusiness.org/
-#'
-#'Data for up to 1990 are sourced from Carbon Dioxide Information Analysis Center, Environmental Sciences Division, Oak Ridge National Laboratory, Tennessee, United States. Data from 1990 are CAIT data: Climate Watch. 2020. GHG Emissions. Washington, DC: World Resources Institute. Available at: https://www.climatewatchdata.org/ghg-emissions.
-#'
-#'International Comparison Progrdevam, World Bank | World Development Indicators database, World Bank | Eurostat-OECD PPP Programme.
-#'
-#'International Comparison Program, World Bank | World Development Indicators database, World Bank | Eurostat-OECD PPP Programme.
-#'
-#'Data for up to 1990 are sourced from Carbon Dioxide Information Analysis Center, Environmental Sciences Division, Oak Ridge National Laboratory, Tennessee, United States. Data from 1990 are CAIT data: Climate Watch. 2020. GHG Emissions. Washington, DC: World Resources Institute. Available at: https://www.climatewatchdata.org/ghg-emissions. See NY.GDP.MKTP.PP.KD for the denominator's source.
-
 "sa_gdp_elec"
