c74c7f02647046b60782488e178a594178e72ae6
npaterno <nicholas.paterno@gmail.com>
2021-09-07 13:37:52 -0700

    updated template for documentation and testing

--- TEMPLATES/doc_test.R
+++ TEMPLATES/doc_test.R
@@ -1,38 +1,41 @@
-# Documenting and Testing New Datasets --------------------------------------------------
+# Documenting and Testing New Datasets ----------------------------------------------------
 
-# After you have imported/cleaned the data (TEMPLATES/data_set_name-dataprep.R) and 
-# written the documentation file (TEMPLATES/data-data_set_name.R) you can run the code in
-# this script file to document and test your data. 
+### After you have imported/cleaned the data (TEMPLATES/data_set_name-dataprep.R) and 
+### written the documentation file (TEMPLATES/data-data_set_name.R) you can run the code
+### in this script file to document and test your data. Please do not save this file after
+### editing for your dataset. If you do, change the name of the file and do NOT add it to 
+### the commit. 
 
-# Documenting New Datasets --------------------------------------------------------------
+# Documenting New Datasets ----------------------------------------------------------------
 
-# Step 1: Restart R using keyboard shortcuts or the code below.
-## Windows: ctrl-shift-F10
-## Mac: cmd-shift-F10
+## Step 1: Restart R using keyboard shortcuts or under 'Session' in the menu bar.
+### Windows: ctrl-shift-F10
+### Mac: cmd-shift-F10
 
-# Step 2: Load the package
+## Step 2: Load the package
 devtools::load_all()
 
-# Step 3: Document
+## Step 3: Document
 devtools::document()
 
-# Step 4: Restart R
+## Step 4: Restart R
 
-# Step 5: Check the help file by replacing name_of_dataset with the name of the new data.
+## Step 5: Check the help file by replacing name_of_dataset with the name of the new data.
 ?name_of_dataset
 
-# If you don't get any errors and everything looks as it should, proceed to testing. 
+## If you don't get any errors and everything looks as it should, proceed to testing. If 
+## you need to make changes, restart this process at step 1.
 
-# Testing New Datasets ------------------------------------------------------------------
+# Testing New Datasets --------------------------------------------------------------------
 
-# Step 1: Restart R
+## Step 1: Restart R
 
-# Step 2: Load the package
+## Step 2: Load the package
 devtools::load_all()
 
-# Step 3: Test the package
+## Step 3: Test the package
 devtools::check()
 
-# The only NOTE you should see as a result of the check should be about the package size. 
-# If any other ERRORs, NOTEs, or WARNINGs are generated, resolve them or open an issue for
-# help.
+## The only NOTE you should see as a result of the check should be about the package size. 
+## If any other ERRORs, NOTEs, or WARNINGs are generated, resolve them or open an issue for
+## help.
